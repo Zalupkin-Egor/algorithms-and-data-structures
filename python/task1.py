d49a1f9f0f004e12c10e8a42f4ccbb4c7b9632b6
Zalupkin-Egor <karamysevv74@gmail.com>
2026-09-10 16:26:48 +0300

    Minor changes

--- python/task1.py
+++ python/task1.py
@@ -3,8 +3,7 @@ import math
 def F(x):
     return 0.1*(x**2) - x * math.log(x)
 
-def pol(L,R,eps):
-
+def pol(L,R,eps):       # Поиск корней методом половинного деления
     if F(L) * F(R)<0:
         while (R-L>eps):
             B = L+(R-L)/2
@@ -15,10 +14,11 @@ def pol(L,R,eps):
         return((L+R)/2)
     else:
         return('Метод неприменим')
-def dF(x):
+
+def dF(x):      # Производная функции
     return 0.2 * x - math.log(x) - 1
 
-def monotonicity(L, R):
+def monotonicity(L, R):     # Проверка на монотонность функции на отрезке
     if all(dF(i/1000)>0 for i in range(L*1000,R*1000+1)):
         return True
     if all(dF(i/1000)<0 for i in range(L*1000,R*1000+1)):
